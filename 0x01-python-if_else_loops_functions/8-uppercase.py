d4ec8223ff2d263e855e88e574beae92eafa9efb
Afia Augustine  Eneji <48561044+austinenergy@users.noreply.github.com>
2022-09-20 22:45:48 +0100

    8 upper commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+
+
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            num = 32
-        else:
-            num = 0
-        print("{:c}".format(ord(str[i]) - num), end='')
-    print()
+    for x in str:
+        if ord(x) >= 97 and ord(x) <= 122:
+            x = chr(ord(x)-32)
+        print('{}'.format(x), end='')
+    print("")
