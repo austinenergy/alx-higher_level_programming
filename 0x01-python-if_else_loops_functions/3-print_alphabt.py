0bf210ee3bce29266479ed313df249da0235da0a
Afia Augustine  Eneji <48561044+austinenergy@users.noreply.github.com>
2022-09-21 04:38:24 +0100

    3 print commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for x in range(ord("a"), ord("z") + 1):
-    if chr(x) != 'q' and chr(x) != 'e':
-        print("{}".format(chr(x)), end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
