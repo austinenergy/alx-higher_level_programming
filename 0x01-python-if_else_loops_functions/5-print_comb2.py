a752e6c60dc0b65848daa21fee961dc580d4e645
Afia Augustine  Eneji <48561044+austinenergy@users.noreply.github.com>
2022-09-21 04:41:32 +0100

    5 print commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("{:0>2d}".format(x), end=", ")
-
-print(x+1)
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
+    else:
+        print("{}{}".format(int(number / 10), number % 10))
